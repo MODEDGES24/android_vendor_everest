1501e7a71a34d93886f59403c87e2cd4f88fd898
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    vendor: Use Pixel charger animation
    Change-Id: I89ac5aa6c4dec0ded4524e94dc07fd268690b5f4
    Signed-off-by: Arseniy Graur <argraur@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,16 +1,6 @@
 # Inherit common mobile Everest stuff
 $(call inherit-product, vendor/everest/config/common.mk)
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_EVEREST_CHARGER),false)
-PRODUCT_PACKAGES += \
-    everest_charger_animation \
-    everest_charger_animation_vendor
-endif
-
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
