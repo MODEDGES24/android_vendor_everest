f6fbe6fd2c0cc7202169cfd65d86f3cc0f6a5b31
Karan Parashar <karan@pixelos.net>
2022-12-01 04:32:54 +0000

    common: Add google dialer call recording feature
    Change-Id: Ifc8ab79aa5bfee81774af96943d668074fc36ee5

--- config/common.mk
+++ config/common.mk
@@ -89,6 +89,13 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_PACKAGES += \
     build-manifest
 
+# Call Recording
+TARGET_CALL_RECORDING_SUPPORTED ?= true
+ifneq ($(TARGET_CALL_RECORDING_SUPPORTED),false)
+PRODUCT_COPY_FILES += \
+    vendor/everest/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+endif
+
 # Everest packages
 PRODUCT_PACKAGES += \
     EverestWallpaperStub
