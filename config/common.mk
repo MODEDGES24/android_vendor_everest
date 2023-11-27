87ba13812776134ffe690123f7c9d34b485c2e71
S I P U N <sipunkumar85@gmail.com>
2023-11-27 03:29:36 +0100

    vendor: Build `Gamespace`
    Change-Id: Ifa481d42623ffb7f0a265680b3d3987a3f3f116f
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -250,6 +250,9 @@ PRODUCT_PACKAGES += \
     NetworkStackOverlay \
     ThemedIconsOverlay
 
+PRODUCT_PACKAGES += \
+    GameSpace
+
 include vendor/everest/config/branding.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
