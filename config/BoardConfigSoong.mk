bananadroid/android_vendor_banana

soong: Add support for miui camera mode [2/2]
Change-Id: I0c912a1c7056bb204a24bd5f0a08308cae71f3f3

config/BoardConfigSoong.mk
@@ -34,8 +34,7 @@ SOONG_CONFIG_bananaGlobalVars += \
     camera_override_format_from_reserved \
     camera_uses_newer_hidl_override_format \
     gralloc_handle_has_custom_content_md_reserved_size \
-    camera_needs_miui_camera_mode_support \
-    camera_needs_camera_needs_depth_sensor_override \
+    uses_miui_camera \
     gralloc_handle_has_reserved_size \
     camera_needs_client_info_lib \
     camera_needs_client_info_lib_oplus \
@@ -92,8 +91,7 @@ SOONG_CONFIG_bananaGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_
 SOONG_CONFIG_bananaGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
-SOONG_CONFIG_bananaGlobalVars_camera_needs_miui_camera_mode_support := $(TARGET_USES_MIUI_CAMERA)
-SOONG_CONFIG_bananaGlobalVars_camera_needs_camera_needs_depth_sensor_override := $(TARGET_USES_DEPTHSENSOR_OVERRIDE)
+SOONG_CONFIG_bananaGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_bananaGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_bananaGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)

MODEDGES24/android_vendor_everest

Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -55,6 +55,7 @@ SOONG_CONFIG_everestGlobalVars += \
     target_trust_usb_control_disable \
     uses_egl_display_array \
     uses_legacy_fd_fbdev \
+    uses_miui_camera \
     uses_nothing_camera \
     uses_oplus_camera
 
@@ -92,6 +93,7 @@ SOONG_CONFIG_everestGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_F
 SOONG_CONFIG_everestGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
 SOONG_CONFIG_everestGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_everestGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
+SOONG_CONFIG_everestGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0